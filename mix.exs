5f9993049de8cff75ea286f4c48607309cace791
Kentaro Kuribayashi <kentarok@gmail.com>
2021-05-11 00:55:13 +0900

    add docs and deps

--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule OffBroadwayOtpDistribution.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "A OTP distribution connector for Broadway"
+  @repo_url "https://github.com/kentaro/off_broadway_otp_distribution"
+
   def project do
     [
       app: :off_broadway_otp_distribution,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.12-rc",
+      name: "OffBroadwayOtpDistribution",
+      description: @description,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package()
     ]
   end
 
@@ -21,8 +29,27 @@ defmodule OffBroadwayOtpDistribution.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:broadway, "~> 0.6.0"},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
+  end
+
+  defp docs do
+    [
+      main: "OffBroadwayOtpDistribution.Producer",
+      nest_modules_by_prefix: [OffBroadwayOtpDistribution],
+      source_ref: @version,
+      source_url: @repo_url,
+      extras: [
+        "README.md"
+      ]
+    ]
+  end
+
+  defp package do
+    %{
+      licenses: ["MIT"],
+      links: %{"GitHub" => @repo_url}
+    }
   end
 end
