2eef26b1b1ab201428e25f4192e854c62d787a59
Kentaro Kuribayashi <kentarok@gmail.com>
2021-06-21 00:32:02 +0900

    implement client module

--- examples/client.ex
+++ examples/client.ex
@@ -1,31 +1,23 @@
 defmodule ExamplesClient do
-  use GenServer
-  require Logger
+  use OffBroadwayOtpDistribution.Client
 
-  @server_name :server@localhost
-  @receiver_name :off_broadway_otp_distribution_receiver
-  @max_retry_count 10
+  @doc """
+  ## `:request_message`
 
-  def start(_type, args) do
-    opts = [strategy: :one_for_one, name: ExamplesClient.Supervisor]
-    [
-      {__MODULE__, args},
-    ] |> Supervisor.start_link(opts)
-  end
+  If the `OffBroadwayOtpDistribution.Producer` runs on `:pull` mode,
+  the producer send `:request_message` to the client.
 
+  ## `:push_message`
+
+  If the `OffBroadwayOtpDistribution.Producer` runs on `:push` mode,
+  you can freely push a message regardless of whether the producer has demand or not.
+  """
   @impl GenServer
-  def init(opts \\ []) do
-    connect_to_node(opts[:node_name])
-    receiver = connect_to_receiver(@receiver_name, @max_retry_count)
+  def handle_cast(:request_message, state) do
+    Logger.info("received: :request_message")
+    GenServer.cast(state.receiver, {:respond_to_pull_request, "I'm alive!"})
 
-    {:ok,
-     %{
-       receiver: receiver
-     }}
-  end
-
-  def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+    {:noreply, state}
   end
 
   @impl GenServer
@@ -34,52 +26,27 @@ defmodule ExamplesClient do
     {:noreply, state}
   end
 
-  @impl GenServer
-  def handle_cast(:request_message, state) do
-    Logger.info("request_message")
-    GenServer.cast(state.receiver, {:respond_to_pull_request, "I'm alive!"})
+  # Helper API
 
-    {:noreply, state}
+  @doc """
+  You must put the client under a supervison of `Supervisor` in case that
+  the client process terminates.
+  """
+  def start(opts \\ []) do
+    [
+      {__MODULE__, opts}
+    ]
+    |> Supervisor.start_link(
+      strategy: :one_for_one,
+      name: ExamplesClient.Supervisor
+    )
   end
 
-  @impl GenServer
-  def handle_info({:DOWN, _, _, _, reason}, state) do
-    Logger.info("server down (#{reason})}")
-    {:noreply, %{state | receiver: nil}}
-  end
-
-  @impl GenServer
-  def terminate(reason, state) do
-    Logger.info("terminating... (#{inspect(reason)})")
-  end
-
-  defp connect_to_node(node_name) do
-    unless Node.connect(node_name) do
-      raise "Could not connect to the server"
-    end
-  end
-
-  defp connect_to_receiver(receiver_name, retry_count) do
-    receiver = try_connect_to_receiver(receiver_name, retry_count)
-    GenServer.call(receiver, :register)
-    Process.monitor(receiver)
-    receiver
-  end
-
-  defp try_connect_to_receiver(receiver_name, retry_count) do
-    if retry_count > 0 do
-      :global.sync()
-      receiver = :global.whereis_name(receiver_name)
-
-      if receiver == :undefined do
-        Logger.debug("waiting for receiver is up...")
-        Process.sleep(3_000)
-        try_connect_to_receiver(receiver_name, retry_count - 1)
-      else
-        receiver
-      end
-    else
-      raise("couldn't connect to #{receiver_name}")
-    end
+  @doc """
+  You can push a message via message casting regardless whether of the Broadway producer has demand or not,
+  if the `OffBroadwayOtpDistribution.Producer` runs on `:push` mode,
+  """
+  def push_message(message) do
+    GenServer.cast(__MODULE__, {:push_message, message})
   end
 end
---
+++ lib/off_broadway_otp_distribution/client.ex
@@ -0,0 +1,118 @@
+defmodule OffBroadwayOtpDistribution.Client do
+  @doc """
+  A base module for implementing the client for `OffBroadwayOtpDistribution.Receiver`.
+
+  ## Example
+
+  The client process implemented using this module communicates to the receiver process
+  implemented as `OffBroadwayOtpDistribution.Receiver` via message passings each other.
+
+  If the producer implemented as `OffBroadwayOtpDistribution.Producer` runs on `:pull` mode
+  and the demand it has is not fully met,
+  it sends `:request_message` message to the client via the receiver.
+  You must implement a callback for the message if the producer runs on `:pull` mode.
+
+  If the producer runs on `:push` mode, you can freely push a message
+  regardless of whether the Broadway producer has demand or not.
+
+  ```
+  defmodule ExamplesClient do
+    use OffBroadwayOtpDistribution.Client
+
+    @impl GenServer
+    def handle_cast(:request_message, state) do
+      Logger.info("received: :request_message")
+      GenServer.cast(state.receiver, {:respond_to_pull_request, "I'm alive!"})
+
+      {:noreply, state}
+    end
+
+    @impl GenServer
+    def handle_cast({:push_message, message}, state) do
+      GenServer.cast(state.receiver, {:push_message, message})
+      {:noreply, state}
+    end
+
+    def start(opts \\ []) do
+      [
+        {__MODULE__, opts}
+      ]
+      |> Supervisor.start_link(
+        strategy: :one_for_one,
+        name: ExamplesClient.Supervisor
+      )
+    end
+
+    def push_message(message) do
+      GenServer.cast(__MODULE__, {:push_message, message})
+    end
+  end
+  ```
+  """
+
+  defmacro __using__(_opts) do
+    quote do
+      use GenServer
+      require Logger
+
+      @default_receiver_name :off_broadway_otp_distribution_receiver
+      @default_max_retry_count 10
+
+      @impl GenServer
+      def init(opts \\ []) do
+        receiver_name = opts[:receiver_name] || @default_receiver_name
+        max_retry_count = opts[:max_retry_count] || @default_max_retry_count
+        receiver = connect_to_receiver(receiver_name, max_retry_count)
+
+        {:ok,
+         %{
+           receiver: receiver
+         }}
+      end
+
+      def start_link(opts \\ []) do
+        GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+      end
+
+      @impl GenServer
+      def handle_info({:DOWN, _, _, _, reason}, state) do
+        Logger.info("Server is down: #{reason}")
+        {:noreply, %{state | receiver: nil}}
+      end
+
+      @impl GenServer
+      def terminate(reason, state) do
+        Logger.info("Client is terminating: #{inspect(reason)}")
+      end
+
+      defp connect_to_receiver(receiver_name, retry_count) do
+        receiver = try_connect_to_receiver(receiver_name, retry_count)
+
+        GenServer.call(receiver, :register)
+        Logger.info("Register this client to #{inspect(receiver)}")
+
+        # To reboot this process when the receiver process terminates
+        Process.monitor(receiver)
+
+        receiver
+      end
+
+      defp try_connect_to_receiver(receiver_name, retry_count) do
+        if retry_count > 0 do
+          :global.sync()
+          receiver = :global.whereis_name(receiver_name)
+
+          if receiver == :undefined do
+            Logger.debug("Waiting for the receiver is up.")
+            Process.sleep(500)
+            try_connect_to_receiver(receiver_name, retry_count - 1)
+          else
+            receiver
+          end
+        else
+          raise("Couldn't connect to #{receiver_name}")
+        end
+      end
+    end
+  end
+end
